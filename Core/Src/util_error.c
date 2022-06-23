cebdf7f4836a4df39ce043be75a36ca193418de1
CallumNunesVaz <callum.nunes-vaz@hotmail.com>
2022-06-24 00:00:38 +1000

    Still broken. But compiles . IRQs fixed by using core_cm3.h.

---
+++ Core/Src/util_error.c
@@ -0,0 +1,17 @@
+/**
+
+ */
+
+#include "util_error.h"
+
+int dbg_log_init(void){
+
+}
+
+void dbg_log(DBG_CODE_TYPE type, DBG_CODE_CONTENT content, const char *lib_name, const size_t lib_name_len){
+    
+}
+
+void dbg_log_code(dbg_code_t code){
+    
+}
