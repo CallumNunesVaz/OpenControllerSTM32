ce04273bf71a3532e8a00b2508759878a421312f
CallumNunesVaz <callum.nunes-vaz@hotmail.com>
2022-06-24 22:37:50 +1000

    Broken due to ASSERT. Up to i2c debug.

---
+++ Core/Inc/drv_gpioexp.h
@@ -0,0 +1,23 @@
+#ifndef __DRV_GPIOEXP_H
+#define __DRV_GPIOEXP_H
+
+#include "mgr_i2c.h"
+
+#include "util_error.h"
+#include "util_buffer.h"
+#include <stdlib.h>
+#include <stdint.h>
+ 
+#define GPIOEXP_I2C_TX_BUF_LEN 0x0F
+#define GPIOEXP_I2C_RX_BUF_LEN 0x0F
+
+#define PCF8574_ADDR 0x40
+
+void gpioexp_init(void);
+
+void gpioexp_set_pin(uint8_t pin);
+
+void gpioexp_get_pin(uint8_t pin);
+
+
+#endif
---
+++ Core/Src/drv_gpioexp.c
@@ -0,0 +1,37 @@
+#include "drv_gpioexp.h"
+
+static uint8_t test_msg[] = {PCF8574_ADDR | I2C_WRITE_BIT, 0xFF};
+
+static uint8_t buf_tx_data[GPIOEXP_I2C_TX_BUF_LEN];
+static uint8_t buf_rx_data[GPIOEXP_I2C_RX_BUF_LEN];
+
+void gpioexp_init(void){
+    /* initialise buffers */
+    gpioexp_set_pin(4);
+}
+
+void gpioexp_set_pin(uint8_t pin) {
+    i2c_msg_t msg;
+
+    /* */
+    if (0x07 < pin) {
+        return;
+    }
+
+    /* construct message */
+    msg.n_send = 2;
+    msg.n_recv = 0;
+    msg.buf_send_ptr = buf_tx_data;
+    msg.buf_send_ptr = buf_rx_data;
+
+    /* write tx buffer */
+    buf_tx_data[0] = PCF8574_ADDR | I2C_WRITE_BIT;
+    buf_tx_data[1] = 0x01 << pin;
+
+    i2c_queue_msg(&msg);
+}
+
+void gpioexp_get_pin(uint8_t pin) {
+
+}
+
